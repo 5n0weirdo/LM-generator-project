71b89c27187f52b703969d8761697dd146f0977b
5n0weirdo <snoweirdo@protonmail.com>
2021-11-16 18:28:27 +0200

    Added form label

--- src/App.js
+++ src/App.js
@@ -3,10 +3,17 @@ import data from "./data";
 function App() {
   const [count, setCount] = useState(0);
   const [text, setText] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Submit");
+  };
   return (
     <div>
       <section className="section-center">
         <h3>Tired of boring lorem ipsum?</h3>
+        <form className="lorem-form" onSubmit={handleSubmit}>
+          <label htmlFor="amount">paragraphs:</label>
+        </form>
       </section>
     </div>
   );
